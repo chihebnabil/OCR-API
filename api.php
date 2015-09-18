9ea48ba10e5f8da1ca765a297ab93e3388ea7908
Chiheb Nabil <chiheb.design@gmail.com>
2015-09-18 17:42:33 +0100

    Update

--- api.php
+++ api.php
@@ -1,14 +1,45 @@
 <?php
-
-
 // Constantes
 define('TARGET', "http://vps197374.ovh.net/ocr/api.php");    // Repertoire cible
 define('MAX_SIZE', 1500000);    // Taille max en octets du fichier
 define('WIDTH_MAX', 3000);    // Largeur max de l'image en pixels
 define('HEIGHT_MAX', 3000);  // Hauteur max de l'image en pixels
 define('API_KEY', "1554383091442022");
-
 ?>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>Reconnaissance optique de caractères</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="#">OCR</a>
+    </div>
+  </div>
+</nav>
+
+<div class='container'>
+<div class="container-fluid">
+
+<div class="row">
+
+	<h2>Reconnaissance optique de caractères</h2>
+	<h4>OCR -- Image 2 Text</h4>
+</div>
+<div class="row">
+	<p>
+		OCR  ( Optical Character Recognition ) ,service gratuit en ligne , permet d'analyser le texte dans tout fichier d'image que vous téléchargez , puis convertir le texte de l'image dans le texte que vous pouvez facilement modifier sur votre ordinateur
+
+	</p>
+</div>
 
 <form enctype="multipart/form-data" action="<?php echo TARGET; ?>" method="post">
  <fieldset>
@@ -29,3 +60,11 @@ define('API_KEY', "1554383091442022");
        </p>
    </fieldset>
  </form>
+ <footer class="footer">
+         <div class="container">
+           <p class="text-muted">2015 CNWEB. contact@cnweb.dz</p>
+         </div>
+       </footer>
+   </body>
+   </html>
+ 
